04923eb17344b1ef17fa455ae195f265d16b7794
Stas Kushnir <stas.kushnir@gmail.com>
2023-01-08 18:05:01 +0300

    remove route from web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\SantaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,5 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/santa/{id}', [SantaController::class, 'index']);
